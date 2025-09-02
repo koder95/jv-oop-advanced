5fc9e033a49a4d102708f0782db3a4442bd5716b
Kamil Jan Mularski <kammul@outlook.com>
2025-09-02 21:08:41 +0200

    Create IsoscelesTrapezoid.java

---
+++ src/main/java/core/basesyntax/IsoscelesTrapezoid.java
@@ -0,0 +1,50 @@
+package core.basesyntax;
+
+public class IsoscelesTrapezoid extends Figure implements Drawable {
+    private double firstBase;
+    private double secondBase;
+    private double height;
+
+    public IsoscelesTrapezoid(double firstBase, double secondBase, double height) {
+        this.firstBase = firstBase;
+        this.secondBase = secondBase;
+        this.height = height;
+    }
+
+    public double getFirstBase() {
+        return firstBase;
+    }
+
+    public void setFirstBase(double firstBase) {
+        this.firstBase = firstBase;
+    }
+
+    public double getSecondBase() {
+        return secondBase;
+    }
+
+    public void setSecondBase(double secondBase) {
+        this.secondBase = secondBase;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: isosceles trapezoid, area: " + getArea()
+                + " sq. units, first base: " + getFirstBase()
+                + " units, second base: " + getSecondBase() + " units, height: " + getHeight()
+                + " units, color: " + getColor().name().toLowerCase());
+    }
+
+    @Override
+    public double getArea() {
+        return 0.5 * (getFirstBase() + getSecondBase()) / getHeight();
+    }
+}
