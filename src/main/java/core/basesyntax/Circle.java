e8a2cb73b1a2d9eb5825282f796213696fa6b607
Kamil Jan Mularski <kammul@outlook.com>
2025-09-02 20:56:44 +0200

    Create Circle.java

---
+++ src/main/java/core/basesyntax/Circle.java
@@ -0,0 +1,29 @@
+package core.basesyntax;
+
+public class Circle extends Figure implements Drawable {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: circle, area: " + getArea() + " sq. units, radius: " + getRadius()
+                + " units, color: " + getColor().name().toLowerCase());
+    }
+
+    @Override
+    public double getArea() {
+        double radius = getRadius();
+        return Math.PI * radius * radius;
+    }
+}
