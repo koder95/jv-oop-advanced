8cda53b16b7899bb12123f2f9f9a6ab4224e352c
Kamil Jan Mularski <kammul@outlook.com>
2025-09-02 18:12:24 +0200

    Add color field

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
 public abstract class Figure {
+    private Color color;
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
     public abstract double getArea();
 }
