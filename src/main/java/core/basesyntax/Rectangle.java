020dd3711daff3a58947381e1c17a089b6542f9a
Kamil Jan Mularski <kammul@outlook.com>
2025-09-02 20:38:46 +0200

    Create Rectangle.java

---
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -0,0 +1,39 @@
+package core.basesyntax;
+
+public class Rectangle extends Figure implements Drawable {
+    private double firstSide;
+    private double secondSide;
+
+    public Rectangle(double firstSide, double secondSide) {
+        this.firstSide = firstSide;
+        this.secondSide = secondSide;
+    }
+
+    public double getFirstSide() {
+        return firstSide;
+    }
+
+    public double getSecondSide() {
+        return secondSide;
+    }
+
+    public void setFirstSide(double firstSide) {
+        this.firstSide = firstSide;
+    }
+
+    public void setSecondSide(double secondSide) {
+        this.secondSide = secondSide;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: rectangle, area: " + getArea() + " sq. units, first side: "
+                + getFirstSide() + " units, second side: " + getSecondSide() + " units, color: "
+                + getColor().name().toLowerCase());
+    }
+
+    @Override
+    public double getArea() {
+        return getFirstSide() * getSecondSide();
+    }
+}
